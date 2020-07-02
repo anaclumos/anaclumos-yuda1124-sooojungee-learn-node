645ca4d03e1f9fcac491fa74dec1fb39114e7f68
woowahan7 <woowahan7@woowahan7ui-MacBookPro.local>
2020-07-02 17:25:44 +0900

    파일 읽어오기 추가

--- task1/app.js
+++ task1/app.js
@@ -4,19 +4,25 @@ http를 사용하지 않고 net 모듈만으로 구현한다.
 */
 
 var net = require("net");
+var fs = require('fs');
 
 let server = net.createServer(sock => {
   let client = sock.remoteAddress;
   console.log('serving stream to ' + client);
+    fs.readFile('public/index.html', function (err, data) {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        sock.write(
+            'HTTP/2.0 200 OK\r\n' +
+            '\r\n'
+        );
 
+        sock.write(data);
+        sock.end();
+    });
   // Headers:
-  sock.write(
-    'HTTP/2.0 200 OK\r\n' +
-    '\r\n'
-  );
-
-  sock.write('Hello world');
-  sock.end();
 });
 
 server.listen(8080);
