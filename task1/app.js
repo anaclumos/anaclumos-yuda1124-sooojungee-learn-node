a1c6c8a048b38ec3c17c225b1f0ff4669168532e
Sunghyun Cho <mail@chosunghyun.com>
2020-07-02 17:17:55 +0900

    Add HTTP Header response

--- task1/app.js
+++ task1/app.js
@@ -3,3 +3,20 @@ http://localhost:8080/index.html 로 접속했을 때 public 디렉토리의 ind
 http를 사용하지 않고 net 모듈만으로 구현한다.
 */
 
+var net = require("net");
+
+let server = net.createServer(sock => {
+  let client = sock.remoteAddress;
+  console.log('serving stream to ' + client);
+
+  // Headers:
+  sock.write(
+    'HTTP/2.0 200 OK\r\n' +
+    '\r\n'
+  );
+
+  sock.write('Hello world');
+  sock.end();
+});
+
+server.listen(8080);
